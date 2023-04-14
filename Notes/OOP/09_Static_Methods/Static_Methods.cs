63f44b9d20fd2fc8dca2df63e58aa06b85b4d5d3
Roman-Ojha <razzroman98@gmail.com>
2023-04-14 19:54:39 +0545

    fix: some error while executing program

--- Notes/OOP/09_Static_Methods/Static_Methods.cs
+++ Notes/OOP/09_Static_Methods/Static_Methods.cs
@@ -10,24 +10,31 @@ namespace StaticMethods
             -> Static methods are declared with the static keyword.
             -> <class_name>.<method_name> is used to call a static method.
     */
-    class User{
-        public string Name { get; set; }
+    class User
+    {
+        public string Name { get; set; } = "";
 
-        public static void PrintUser(User user){
+        public static void PrintUser(User user)
+        {
             // because this is the statice method this can't have the access of this object.
             // so it have to get the user object as an parameter.
             Console.WriteLine("User Name: {0}", user.Name);
         }
 
-        public static void PrintUsers(List<User> users){
-            forEach(User user in users){
+        public static void PrintUsers(List<User> users)
+        {
+            foreach (User user in users)
+            {
                 Console.WriteLine("User Name: {0}", user.Name);
             }
+
         }
     }
-    class Prog{
+    class Prog
+    {
 
-        static public void Run(string[] args){
+        static public void Run(string[] args)
+        {
             // So here we are in the class 'Prog' and 'Run' is the main function that get called when the program starts.
             // but even having the 'Run' function inside the 'Prog' class, we can't call 'DoSomething' function without creating an instance of the class.
             // Because 'DoSomething' is the instance method and only be able to call using instance object.
@@ -36,7 +43,8 @@ namespace StaticMethods
             p.DoSomething();
         }
 
-        public void DoSomething(){
+        public void DoSomething()
+        {
             Console.WriteLine("Doing Something");
             User user = new User();
             user.Name = "Roman Ojha";
@@ -45,8 +53,8 @@ namespace StaticMethods
             User.PrintUser(user);
 
             List<User> users = new List<User>();
-            users.Add(new User(){Name = "Roman Ojha"})
-            users.Add(new User(){Name = "Razz Roman"})
+            users.Add(new User() { Name = "Roman Ojha" });
+            users.Add(new User() { Name = "Razz Roman" });
         }
     }
 }
