cd683a130e78f14aa8cda7433fc681b2d14a0dc5
Roman-Ojha <razzroman98@gmail.com>
2023-05-06 06:30:06 +0545

    feat: small changes in college lab01

--- College/Lab/Lab_01/Q4.cs
+++ College/Lab/Lab_01/Q4.cs
@@ -6,20 +6,20 @@ namespace Lab01Q4
     {
         static public void Run(string[] args)
         {
-            string path = @"C:\MyFolder";
+            string path = @"D:\CsharpExample";
 
-            // Create a directory (folder) in the specified path
+            // Create a directory:
             Directory.CreateDirectory(path);
 
-            // Create a file in the directory
-            string filePath = Path.Combine(path, "MyFile.txt");
+            // Create a file:
+            string filePath = Path.Combine(path, "hello.txt");
             File.WriteAllText(filePath, "Hello, world!");
 
-            // Select the file and print its contents
+            // Select the file and print its contents:
             string fileContents = File.ReadAllText(filePath);
             Console.WriteLine(fileContents);
 
-            // Select the directory and print its contents
+            // Select the directory and print its contents:
             DirectoryInfo directoryInfo = new DirectoryInfo(path);
             Console.WriteLine($"Directory: {directoryInfo.FullName}");
             foreach (var file in directoryInfo.GetFiles())
