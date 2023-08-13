a6866c240f08a9a7d02450f0986e668635d67b8b
Roman-Ojha <razzroman98@gmail.com>
2023-08-13 20:31:33 +0545

    feat(practice): loops, jaggad array, class, fields, property, indexer

--- Notes/Csharp_Basic/02_Hello_world/Hello_World.cs
+++ Notes/Csharp_Basic/02_Hello_world/Hello_World.cs
@@ -2,8 +2,10 @@ using System;
 
 namespace HelloWorld
 {
-    class Prog{
-        static public void Run(string[] args){
+    class Prog
+    {
+        static public void Run(string[] args)
+        {
             Console.WriteLine("Hello roman");
         }
     }
